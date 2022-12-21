49566b0d4f82bad8622e683511ac4dab2d5f821d
TY <ddsamidare@gmail.com>
2022-12-21 22:13:32 +0900

    set color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,17 @@ module.exports = {
   ],
   darkMode: 'media', // or 'class'
   theme: {
+    colors: {
+      green: '#016461',
+      lightgreen: '#019585',
+      darkblue: '#002130',
+      gray: '#292929',
+      lightgray: '#E6E6E6',
+      red: '#C40026',
+      white: '#ffffff',
+      orange: '#FF7347',
+      transparent: 'transparent'
+    },
     extend: {}
   },
   variants: {
