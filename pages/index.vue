fe0b20282413eb4a33f315cf566220d571ec2631
TY <ddsamidare@gmail.com>
2022-12-23 00:17:38 +0900

    ページヘッダーを分離

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="w-full grid grid-cols-1 text-white">
-    <h1
-      class="border-solid border-white border-l-4 text-xl pl-4"
-    >
-      Latest Topics
-    </h1>
-    <article v-for="i in 5" :key="i" class="w-full h-80 bg-darkblue my-2 p-4">
+    <PageHeader>
+      Latest topics
+    </PageHeader>
+    <section v-for="i in 5" :key="i" class="w-full h-80 bg-darkblue my-2 p-4">
       <h3>Empty article-{{ i }}</h3>
-    </article>
+    </section>
   </div>
 </template>
 
 <script setup>
+import PageHeader from '~~/components/client/PageHeader.vue'
 
 useHead({
   title: 'WIP|poripority.com'
