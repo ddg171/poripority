da9bed4a523371621516eb8557edc315f1dffe93
T.Y <tomyamamt@gmail.com>
2022-12-23 14:43:44 +0900

    マージン削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen flex flex-col ">
     <MyHeader />
     <div class="w-full min-h-screen flex  flex-col  items-center justify-center">
-      <main class="w-full flex bg-transparent max-w-4xl m-2 lg:m-4 ">
+      <main class="w-full flex bg-transparent max-w-4xl ">
         <slot />
       </main>
       <footer class="border-t-solid border-t-4 border-red w-full h-60 bg-lightgray ">
