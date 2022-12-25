3906b8552ab434f5c5e2e1dca27a2983ecfdb822
TY <ddsamidare@gmail.com>
2022-12-25 10:43:16 +0900

    今のところ使わない変数を削除

--- app.vue
+++ app.vue
@@ -1,20 +1,12 @@
 <template>
   <div id="app" class="min-h-screen bg-gray">
-    <NuxtLayout :categories="categories">
+    <NuxtLayout>
       <NuxtPage @click-img="imgClick" />
     </NuxtLayout>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { LinkParams } from '~~/types'
-
-const categories = ref<LinkParams[]>(
-  [
-    { name: 'Game', path: '/blog' },
-    { name: 'camera', path: '/blog' },
-    { name: 'others', path: '/blog' }]
-)
 
 const imgUrl = ref<string|null>(null)
 
