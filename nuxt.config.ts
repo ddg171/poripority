04f4c49696e183982984edf76ded61ddd0ebd021
TY <ddsamidare@gmail.com>
2022-12-25 10:42:23 +0900

    update nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,10 @@
-import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-
+import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
-  env: {
-    NODE_ENV: process.env.NODE_ENV
-  },
   app: {
     head: {
       charset: 'utf-8',
-      lang: 'ja',
+      // lang: 'ja',
       viewport: 'width=device-width, initial-scale=1',
       meta: [{ name: 'robots', content: 'noindex,nofollow' }],
       link: [{ rel: 'icon', href: '/favicon.ico', id: 'favicon' }],
@@ -17,31 +12,16 @@ export default defineNuxtConfig({
       script: []
     }
   },
-  server: {
-    port: Number(process.env.PORT) || 3000,
-    host: process.env.HOST || '0.0.0.0',
-    timing: false
-  },
-  privateRuntimeConfig: {
-    private: process.env.PRIVATE_KEY,
+  runtimeConfig: {
     microCMSAPI: process.env.MICROCMS_API,
     microCMSServiceID: process.env.MICROCMS_SERVEICE_ID
-  },
-  autoImports: {
-    dirs: ['/types']
   },
   typescript: {
     shim: false
   },
   css: ['~/assets/css/tailwind.scss'],
+  modules: ['@nuxtjs/tailwindcss'],
   build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {}
-        }
-      }
-    }
+
   }
 })
