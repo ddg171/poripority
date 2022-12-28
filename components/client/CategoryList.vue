287de727ea032c9b75d0547c5282d3129867d5eb
TY <ddsamidare@gmail.com>
2022-12-29 00:41:19 +0900

    カテゴリ一覧を仮作成

---
+++ components/client/CategoryList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ClientHeader3>カテゴリ</ClientHeader3>
+    <ul>
+      <li v-for="c ,i in categories" :key="i" class="w-full text-lg hover:bg-green focus-within:bg-green p-2 md:my-1">
+        <NuxtLink :to="`/blog?category=${c.id}`" class="w-full h-full hover:underline focus:underline" tabindex="1">
+          {{ c.name }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">import { Category } from '~~/types'
+
+const { data } = await useFetch('/api/category')
+
+const contents = data.value?.contents || []
+const categories = reactive<Array<Category>>(contents)
+</script>
