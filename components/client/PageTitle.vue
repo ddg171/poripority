7251b96178a2a7538c7df5eae1e9207f16ddec26
TY <ddsamidare@gmail.com>
2022-12-29 21:27:15 +0900

    シンプルな構成に変更

--- components/client/PageTitle.vue
+++ components/client/PageTitle.vue
@@ -1,32 +1,28 @@
 <template>
   <div class="w-full mb-4 overflow-hidden top-slider-wrapper">
-    <div class="relative w-full h-1/4h lg:h-1/3h">
+    <div class="relative w-full h-1/4h">
       <div class="relative w-full h-full page-title">
-        <div class="absolute w-full h-full">
-          <div class="relative w-full h-full top-slider-content">
-            <div
-              v-if="img"
-              class="absolute flex items-center justify-center w-full h-full mb-6 overflow-hidden firstview bg-darkblue"
-            >
-              <PictureBox
-                :souce="img.souce"
-                :alt="img.alt"
-                :title="img.title"
-                :webp="img.webp"
-                :jpg="img.jpg"
-              />
-            </div>
-            <div class="static w-full h-full title-box md:absolute backdrop-blur-sm">
-              <div class="relative w-full h-full">
-                <div class="absolute top-0 flex flex-col items-center justify-center w-full h-full p-4 ml-0 md:w-auto sm:h-auto sm:top-8 md:top-1/3 sm:ml-1 md:ml-16 lg:ml-20 bg-green/75">
-                  <h1 class="flex items-center pb-1 pl-2 mb-0 text-xl text-white border-l-8 border-solid md:text-5xl sm:mb-2 border-l-lightgray">
-                    <span>{{ title }}</span>
-                  </h1>
-                  <p v-for="t , i in subtitles" :key="i" class="text-white text-md md:text-xl">
-                    {{ t }}
-                  </p>
-                </div>
-              </div>
+        <div
+          v-if="img"
+          class="absolute flex items-center justify-center w-full h-full overflow-hidden firstview "
+        >
+          <PictureBox
+            :souce="img.souce"
+            :alt="img.alt"
+            :title="img.title"
+            :webp="img.webp"
+            :jpg="img.jpg"
+          />
+        </div>
+        <div class="absolute w-full h-full title-box backdrop-blur-sm">
+          <div class="flex items-center justify-center w-full h-full ">
+            <div class="flex flex-col p-4 bg-green/75">
+              <h1 class="flex items-center pb-1 pl-2 mb-0 text-xl text-white border-l-8 border-solid md:text-5xl sm:mb-2 border-l-lightgray">
+                <span>{{ title }}</span>
+              </h1>
+              <p v-for="t , i in subtitles" :key="i" class="text-white text-md md:text-xl">
+                {{ t }}
+              </p>
             </div>
           </div>
         </div>
