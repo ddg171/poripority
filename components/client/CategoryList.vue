b4a9c0f0e8a461a16835fb1581fe259c4758092e
TY <ddsamidare@gmail.com>
2022-12-29 18:46:14 +0900

    リンク追加

--- components/client/CategoryList.vue
+++ components/client/CategoryList.vue
@@ -2,8 +2,8 @@
   <div>
     <ClientHeader3>カテゴリ</ClientHeader3>
     <ul>
-      <li v-for="c ,i in categories" :key="i" class="w-full text-lg hover:bg-green focus-within:bg-green p-2 md:my-1">
-        <NuxtLink :to="`/blog?category=${c.id}`" class="w-full h-full hover:underline focus:underline" tabindex="0">
+      <li v-for="c ,i in categories" :key="i" class="w-full p-2 text-lg hover:bg-green focus-within:bg-green md:my-1">
+        <NuxtLink :to="`/blog?category=${c.id}`" class="w-full h-full hover:underline focus:underline" :replace="true" tabindex="0">
           {{ c.name }}
         </NuxtLink>
       </li>
--- components/client/TopArticleCard.vue
+++ components/client/TopArticleCard.vue
@@ -1,19 +1,19 @@
 <template>
-  <article class="max-w-xl flex flex-col md:flex-row items-start bg-darkblue my-4 mx-0 md:my-2 md:mx-2">
-    <div class="w-full h-48  md:w-48 md:h-48  shrink-0 flex justify-center items-center overflow-hidden">
+  <article class="flex flex-col items-start max-w-xl mx-0 my-4 md:flex-row bg-darkblue md:my-2 md:mx-2">
+    <div class="flex items-center justify-center w-full h-48 overflow-hidden md:w-48 md:h-48 shrink-0">
       <picture class="w-full">
         <source :srcset="cropEyecatch(props.article.eyecatch).url" type="image/webp">
         <img :src=" cropEyecatch(props.article.eyecatch,false).url " height="400" width="400" alt="">
       </picture>
     </div>
-    <div class="h-full ml-2 flex flex-col justify-between">
+    <div class="flex flex-col justify-between h-full ml-2">
       <div>
-        <h2 v-if="props.heading===2" class="text-xl mb-2 md:mb-4">
+        <h2 v-if="props.heading===2" class="mb-2 text-xl md:mb-4">
           <NuxtLink :to="to" class="hover:underline" tabindex="0">
             {{ props.article.title }}
           </NuxtLink>
         </h2>
-        <h3 v-else class="text-xl mb-2 md:mb-4">
+        <h3 v-else class="mb-2 text-xl md:mb-4">
           <NuxtLink :to="to" class="hover:underline" tabindex="0">
             {{ props.article.title }}
           </NuxtLink>
@@ -24,10 +24,14 @@
       </div>
       <div class="mt-1 md:mt-0">
         <p class="text-sm">
-          {{ props.article.createdAt }}
+          {{ publishedAt }}
         </p>
         <p class="text-sm">
-          {{ props.article.category.name }}
+          Category:
+          <NuxtLink v-if="props.article.category" :to="`/blog?category=${props.article.category.id}`" class="hover:underline focus:underline">
+            {{ props.article.category.name }}
+          </NuxtLink>
+          <span v-else>n/a</span>
         </p>
         <blockquote>
           <p />
@@ -38,7 +42,9 @@
 </template>
 
 <script setup lang="ts">
+import { parseISO, format, isValid } from 'date-fns'
 import { cropSquare } from '../imageAPIHelpre'
+import { publishedAtFormat } from '~~/utils/util'
 import { Article } from '~~/types'
 
 interface Props{
@@ -63,6 +69,11 @@ const to = computed<string>(
 
     return params.length ? path + '?' + params.join('&') : path
   })
+
+const publishedAt = computed(() => {
+  const d = parseISO(props.article?.publishedAt)
+  return isValid(d) ? format(d, publishedAtFormat) : 'n/a'
+})
 
 const cropEyecatch = cropSquare
 </script>
