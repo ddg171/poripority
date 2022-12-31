70b7871d4aa5f9aa9fe35e04d13c028c3229e340
TY <ddsamidare@gmail.com>
2022-12-31 23:55:29 +0900

    リンクをおしやすくした

--- components/client/CategoryList.vue
+++ components/client/CategoryList.vue
@@ -3,12 +3,12 @@
     <ClientHeader3>カテゴリ</ClientHeader3>
     <ul>
       <li class="w-full p-2 text-lg hover:bg-green focus-within:bg-green md:my-1">
-        <NuxtLink :to="`/blog`" class="w-full h-full hover:underline focus:underline" :replace="true" tabindex="0">
+        <NuxtLink :to="`/blog`" class="block w-full h-full hover:underline focus:underline" tabindex="0">
           全て
         </NuxtLink>
       </li>
       <li v-for="c ,i in state" :key="i" class="w-full p-2 text-lg hover:bg-green focus-within:bg-green md:my-1">
-        <NuxtLink :to="`/blog?category=${c.id}`" class="w-full h-full hover:underline focus:underline" :replace="true" tabindex="0">
+        <NuxtLink :to="`/blog?category=${c.id}`" class="block w-full h-full hover:underline focus:underline" tabindex="0">
           {{ c.name }}
         </NuxtLink>
       </li>
