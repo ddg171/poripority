3c448bc26b3e4dbd15affb4494f29dd588c5991c
TY <ddsamidare@gmail.com>
2023-01-03 00:44:13 +0900

    制作物紹介コンポーネントを作成

---
+++ components/client/WorksArticle.vue
@@ -0,0 +1,39 @@
+<template>
+  <article :id="props.id" class="mx-0 my-8 md:my-2 md:mx-2">
+    <ClientHeader2>
+      <slot name="name" />
+    </ClientHeader2>
+    <p>
+      状況:
+      <slot name="status" />
+    </p>
+    <div class="w-full flex flex-col">
+      <div class="w-full h-48 bg-green flex justify-center items-start mb-4">
+        <slot name="pic" />
+      </div>
+      <div>
+        <slot name="introduction" />
+      </div>
+      <p class="mt-8">
+        <ul>
+          <li v-for="l , i in links" :key="i">
+            <NuxtLink :to="l.path" class="text-lg hove:underline focus:underline">
+              {{ l.name }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </p>
+    </div>
+  </article>
+</template>
+
+<script setup lang="ts">
+import { LinkParams } from '~~/types'
+
+interface Props{
+    id:string
+    links:LinkParams[]
+}
+
+const props = withDefaults(defineProps<Props>(), { id: '', links: () => [] })
+</script>
