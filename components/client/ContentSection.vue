c1afac39edba0367ed223bfc8fd20ca6bcdb7f05
TY <ddsamidare@gmail.com>
2023-01-07 18:49:34 +0900

    余白調整

--- components/client/ContentSection.vue
+++ components/client/ContentSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="w-full p-4 mb-2 md:p-6 bg-darkblue text-white">
+  <section class="w-full p-6 mb-2 md:py-12 md:px-8 bg-darkblue text-white">
     <slot />
   </section>
 </template>
