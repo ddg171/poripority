f5876521abd67c4d8d69abbfa663bea50fcc7e59
TY <ddsamidare@gmail.com>
2023-01-12 23:26:58 +0900

    文面変更

--- pages/about.vue
+++ pages/about.vue
@@ -2,15 +2,13 @@
   <div class="page-about flex flex-col items-center w-full max-w-screen-xl ">
     <ClientContentSection>
       <ClientHeader1>
-        このWEBサイトと作者について
+        ABOUT ME
       </ClientHeader1>
-      <p>あるいはポートフォリオ</p>
+      <p>WIP</p>
     </ClientContentSection>
   </div>
 </template>
 
 <script setup lang="ts">
-
-// throw createError({ statusCode: 404, statusMessage: 'このページは現在制作中です。' })
 
 </script>
