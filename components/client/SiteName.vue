214e9971e668f21077c31d4653e808944c4739a9
T.Y <tomyamamt@gmail.com>
2023-01-13 15:30:57 +0900

    ページ単位のOGPを設定

--- components/client/SiteName.vue
+++ components/client/SiteName.vue
@@ -1,9 +1,17 @@
 <template>
   <h1 id="site-name" class="site-name text-xl md:text-3xl text-white italic flex items-center justify-center ml-2 md:ml-4">
     <a href="/">
-      The hut of Poripority
+      {{ siteName }}
     </a>
   </h1>
 </template>
 
+<script setup lang="ts">
+const config = useRuntimeConfig()
+
+const siteName = computed<string>(() => {
+  return config.public.siteName || 'The hut of Poripority'
+})
+
+</script>
 <style></style>
