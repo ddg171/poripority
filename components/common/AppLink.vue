947a408f2f88974c3bd7ff538520bcab709bf77c
TY <ddsamidare@gmail.com>
2023-01-18 00:24:52 +0900

    [WIP]共通コンポーネントを仮作成

---
+++ components/common/AppLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLink :to="props.to" class="block w-full h-full hover:underline focus:underline" tabindex="0">
+    <span>
+      <slot />
+    </span>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    to?:string
+}
+
+const props = withDefaults(defineProps<Props>(), { to: '' })
+</script>
---
+++ components/common/ExternalLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <a :href="props.to" class="block w-full h-full hover:underline focus:underline" target="_blank" tabindex="0">
+    <span>
+      <slot />
+    </span>
+  </a>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+      to?:string
+  }
+
+const props = withDefaults(defineProps<Props>(), { to: '' })
+</script>
