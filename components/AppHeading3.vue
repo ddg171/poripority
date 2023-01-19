ab7e32c3a6ab493866e3d289347252b07e37aeb1
TY <ddsamidare@gmail.com>
2023-01-20 00:10:52 +0900

    ジャンプ率確保のため文字サイズ調整

--- components/AppHeading3.vue
+++ components/AppHeading3.vue
@@ -1,6 +1,6 @@
 <template>
   <h3
-    class="pl-2 text-xl font-semibold text-white border-l-4 border-white border-solid md:text-2xl"
+    class="pl-2 font-semibold text-white border-l-4 border-white border-solid text-2xl"
   >
     <slot />
   </h3>
