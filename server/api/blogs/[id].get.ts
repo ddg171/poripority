04b557976286731a6cb4aadef1206df6afeb2a39
T.Y <tomyamamt@gmail.com>
2023-01-20 17:12:40 +0900

    エラーハンドリング追加

--- server/api/blogs/[id].get.ts
+++ server/api/blogs/[id].get.ts
@@ -3,10 +3,17 @@ import { Article } from '~~/types/articles'
 
 export default defineEventHandler(async (event):Promise<Article> => {
   const contentId = event.context.params.id
-  const res = await client
-    .get({
-      endpoint: 'blogs',
-      contentId
-    })
-  return res
+  if (!contentId) {
+    throw createError({ statusCode: 400, statusMessage: 'err' })
+  }
+  try {
+    const res = await client
+      .get({
+        endpoint: 'blogs',
+        contentId
+      })
+    return res
+  } catch {
+    throw createError({ statusCode: 404, statusMessage: 'err' })
+  }
 })
