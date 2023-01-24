b671e523afcc964d300f36216f889dc79afbe926
TY <ddsamidare@gmail.com>
2023-01-24 22:51:40 +0900

    NuxtPictureのラッパーコンポーネントに変更

--- components/PictureBox.vue
+++ components/PictureBox.vue
@@ -1,33 +1,28 @@
 
 <template>
-  <picture class="picture-box w-full h-full flex justify-center items-center">
-    <!-- 最大サイズ以外のWebp -->
-    <source
-      v-for="s,i in props.source"
-      :key="i"
-      type="image/webp"
-      media="(max-width:1024px)"
-      :srcset="s"
-      sizes="(max-width: 640px) 450px, 970w"
-    >
-    <!-- 一番大きいWebp -->
-    <source type="image/webp" :srcset="webp">
-    <img
-      class="min-h-full min-w-full object-cover "
-      :src="props.jpg"
-      :alt="props.alt"
-      :title="props.title"
-      decoding="async"
-      height="500"
-      width="500"
-    >
-  </picture>
+  <NuxtPicture
+    :src="props.webp"
+    class="picture-box"
+    legacy-format="jpeg"
+    :img-attrs="imgAttr"
+  />
 </template>
 
 <script setup lang="ts">
 
 interface Props {source?:string[]
-    webp:string, jpg:string, alt:string, title:string}
+    webp:string, jpg?:string, alt?:string, title?:string}
+
+const imgAttr = computed(() => {
+  return {
+    class: 'img-inside-picture',
+    height: 400,
+    width: 400,
+    decoding: 'async',
+    alt: props.alt || '',
+    title: props.title || ''
+  }
+})
 
 const props = withDefaults(defineProps<Props>(), { source: () => [], webp: '', jpg: '', alt: '', title: '' })
 </script>
