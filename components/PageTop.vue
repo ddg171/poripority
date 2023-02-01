e1adea433a8aabff351a549f4e2703c95928939c
TY <ddsamidare@gmail.com>
2023-02-01 23:02:08 +0900

    高さ変更

--- components/PageTop.vue
+++ components/PageTop.vue
@@ -1,10 +1,10 @@
 <template>
-  <TopConteinerBlock class="h-1/3h max-h-720p min-h-360p">
+  <TopConteinerBlock class="h-1/4h lg:h-1/2h max-h-960p min-h-480p">
     <div :data-show="isShow" class="title-content-box relative  h-full page-title transition-all  duration-500 opacity-0">
       <TopImgBlock :img="state.topImg" />
       <div v-if="state.title" class="absolute w-full h-full flex justify-center items-center title-box backdrop-blur-sm">
-        <CommonContentWidthBox class="flex items-center justify-start">
-          <div class="flex flex-col px-4 md:px-16 py-2  md:py-6 bg-green/75 w-full md:w-auto my-2">
+        <CommonContentWidthBox class="flex items-end justify-start">
+          <div class="flex flex-col px-4 md:px-16 py-2  md:py-6 mb-1/5h  md:mb-1/7h bg-green/75 w-full md:w-auto my-2">
             <AppHeading1>
               <span>{{ state.title }}</span>
             </AppHeading1>
