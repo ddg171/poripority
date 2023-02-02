3fe21f751a647a013dabb17afd249f9473daacb2
TY <ddsamidare@gmail.com>
2023-02-02 22:34:54 +0900

    サーバーサイドでcookieが読めないためClientOnlyに変更

--- components/OptIn.vue
+++ components/OptIn.vue
@@ -1,25 +1,29 @@
 <template>
-  <div v-if="isShow" class="optin py-6 px-4 md:px-12 md:max-w-2xl absolute w-full border-solid border-white bottom-0 ml-0 shadow-xl shadow-gray  opacity-100 bg-gray grid gap-2 transition-opacity  delay-600 z-40">
-    <div class="w-full">
-      <AppHeading2>Cookie/解析ツール等の使用について</AppHeading2>
-      <div class="text-white ">
-        <CommonAppApra>
-          本Webサイトではアクセス解析のためGoogle Analytics(以下"GA")を導入しています。GAによる情報送信を回避する場合は、下記の拒否ボタンを押してください。
-        </CommonAppApra>
-        <CommonAppApra>
-          本Webサイトではクッキーを使用しておりますが、オプトイン設定の識別のみに利用し、個人情報の保持・収集には使用しておりません。
-        </CommonAppApra>
+  <ClientOnly>
+    <Transition name="optin">
+      <div v-if="isShow" class="optin py-6 px-4 md:px-12 md:max-w-2xl absolute w-full border-solid border-white bottom-0 ml-0 shadow-xl shadow-gray  opacity-100 bg-gray grid gap-2 transition-opacity  delay-600 z-40">
+        <div class="w-full">
+          <AppHeading2>Cookie/解析ツール等の使用について</AppHeading2>
+          <div class="text-white ">
+            <CommonAppApra>
+              本Webサイトではアクセス解析のためGoogle Analytics(以下"GA")を導入しています。GAによる情報送信を回避する場合は、下記の拒否ボタンを押してください。
+            </CommonAppApra>
+            <CommonAppApra>
+              本Webサイトではクッキーを使用しておりますが、オプトイン設定の識別のみに利用し、個人情報の保持・収集には使用しておりません。
+            </CommonAppApra>
+          </div>
+        </div>
+        <div class="w-full flex justify-around">
+          <CommonAppBtn @click="setOptIn(false)">
+            DENIED/拒否
+          </CommonAppBtn>
+          <CommonAppBtn variant="denied" @click="setOptIn(true)">
+            ACCEPT/許可
+          </CommonAppBtn>
+        </div>
       </div>
-    </div>
-    <div class="w-full flex justify-around">
-      <CommonAppBtn @click="setOptIn(false)">
-        DENIED/拒否
-      </CommonAppBtn>
-      <CommonAppBtn variant="denied" @click="setOptIn(true)">
-        ACCEPT/許可
-      </CommonAppBtn>
-    </div>
-  </div>
+    </Transition>
+  </ClientOnly>
 </template>
 
 <script lang="ts" setup>
@@ -72,3 +76,16 @@ onMounted(() => {
 })
 
 </script>
+
+<style scoped>
+.optin-enter-active{
+  opacity: 1;
+  transition: opacity 0.25s linear;
+  transition-delay: 0.5s;
+}
+
+.optin-enter-from,
+.optin-leave-to {
+  opacity: 0;
+}
+</style>
