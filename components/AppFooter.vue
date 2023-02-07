7aeefc9925dcc2ce847a1459108103fa33534cca
TY <ddsamidare@gmail.com>
2023-02-07 23:30:33 +0900

    URLが入れ替わっている不具合を修正

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -17,27 +17,27 @@
           </h3>
           <ul class="flex gap-2 flex-wrap">
             <li>
-              <CommonExternalLink to="https://twitter.com/hata_kaze">
+              <CommonExternalLink to="https://twitter.com/hata_kaze" title="Twitter">
                 <Icon name="mdi:twitter" class="bg-twttr text-white p-1" />
                 Twitter
               </CommonExternalLink>
             </li>
             <li>
-              <CommonExternalLink to="https://github.com/ddg171">
+              <CommonExternalLink to="https://www.instagram.com/hata_kaze" title="Instagram">
                 <Icon name="mdi:instagram" class="h-4 w-4" />
 
                 Instagram
               </CommonExternalLink>
             </li>
             <li>
-              <CommonExternalLink to="https://www.instagram.com/hata_kaze/">
+              <CommonExternalLink to="https://github.com/ddg171" title="Github">
                 <Icon name="mdi:github" class="h-4 w-4" />
 
                 Github
               </CommonExternalLink>
             </li>
             <li>
-              <CommonExternalLink to="https://qiita.com/ddg171">
+              <CommonExternalLink to="https://qiita.com/ddg171" title="Qiita">
                 Qiita
               </CommonExternalLink>
             </li>
