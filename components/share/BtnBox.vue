28ede4516ed7f74e1b2e5679bb31c5b5035c643c
T.Y <tomyamamt@gmail.com>
2023-03-14 11:38:23 +0900

    footerのシェアボタンのURLを固定

--- components/share/BtnBox.vue
+++ components/share/BtnBox.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="flex gap-1">
-    <ShareTwttrLink :text="props.title" />
-    <ShareRedditLink :text="props.title" />
-    <ShareCopyUrlBtn />
+    <ShareTwttrLink :text="props.title" :url="props.url" />
+    <ShareRedditLink :text="props.title" :url="props.url" />
+    <ShareCopyUrlBtn :url="props.url" />
   </div>
 </template>
 
 <script setup lang="ts">
 interface Props{
     title?:string
+    url?:string
 }
 
 const props = defineProps<Props>()
