2bf5ade4832d3765accff41ab2f1f9047eb4c7e7
T.Y <tomyamamt@gmail.com>
2023-03-16 18:00:53 +0900

    ヘッダー部分の構造を変更

--- components/about/ContentSection.vue
+++ components/about/ContentSection.vue
@@ -1,14 +1,11 @@
 <template>
-  <section class="w-full min-h-screen pb-20">
-    <IntersectionWrapper :threshold="0.5" trantision="vertical" class="w-full h-full bg-darkblue" @in="$emit('in')">
+  <section class="w-full min-h-screen pb-20 snap-start">
+    <IntersectionWrapper :threshold="0.5" trantision="vertical" class="w-full h-full " @in="$emit('in')">
       <div class="flex flex-col items-center w-full">
-        <CommonContentWidthBox class="flex flex-col">
-          <div class="sticky top-0 z-30 w-full pt-20 pb-8 bg-darkblue">
-            <AppHeading2>
-              <slot name="heading" />
-            </AppHeading2>
+        <CommonContentWidthBox class="flex flex-col ">
+          <div class="sticky top-0 w-full pt-20 pb-8 z-100">
+            <slot name="heading" />
           </div>
-
           <slot />
         </CommonContentWidthBox>
       </div>
