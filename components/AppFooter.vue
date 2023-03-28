129abe6e02591df4721f7e937d3d781884851b5f
TY <ddsamidare@gmail.com>
2023-03-29 00:38:45 +0900

    注意書き追加

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -21,6 +21,8 @@
       <div class="text-sm w-full flex flex-col items-start mb-2 md:justify-end md:h-full md:w-auto shrink-0">
         <p>このWebサイトはリンクフリーです。</p>
         <p>サイト内の情報を引用する際は出典元と閲覧日時を明記してください。</p>
+        <p>サイト内の情報の引用によって生じたあらゆる事象について、当サイトの管理者は一切の責任を負いません。</p>
+        <p>サイト内で使用している写真・映像の著作権は、記載のない限り権利者に帰属します。</p>
       </div>
     </div>
   </footer>
