9772a056f3b49decf8be191fd9f632b68531e30c
TY <ddsamidare@gmail.com>
2023-03-28 22:41:52 +0900

    sticky削除

--- components/about/ContentSection.vue
+++ components/about/ContentSection.vue
@@ -3,7 +3,7 @@
     <IntersectionWrapper :threshold="0.5" trantision="vertical" class="w-full h-full " @in="$emit('in')">
       <div class="flex flex-col items-center w-full">
         <CommonContentWidthBox class="flex flex-col px-2 ">
-          <div class="w-full pt-10 md:pt-16 mb-2 md:mb-8 z-40 sticky top-0">
+          <div class="w-full  mb-2 md:mb-8 z-40 ">
             <slot name="heading" />
           </div>
           <slot />
