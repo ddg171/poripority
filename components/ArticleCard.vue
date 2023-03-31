d9aefeb0032591f8ffbd188692172d65f2a4ac2b
TY <ddsamidare@gmail.com>
2023-03-31 23:21:54 +0900

    imgixでの処理に対応

--- components/ArticleCard.vue
+++ components/ArticleCard.vue
@@ -2,10 +2,20 @@
   <article class="flex flex-col items-start md:flex-row bg-darkblue ">
     <div class=" w-full h-60 overflow-hidden md:w-48 md:h-48 shrink-0 bg-darkblue">
       <CommonAppLink :to="to" class="flex items-center justify-start md:justify-center border border-transparent hover:border-white focus:border-white" :title="props.article.title ">
-        <picture v-if="props.article.eyecatch" class="w-full">
+        <!-- <picture v-if="props.article.eyecatch" class="w-full">
           <source :srcset="cropEyecatch(props.article.eyecatch,true,500).url" type="image/webp">
           <img class="w-full" :src=" cropEyecatch(props.article.eyecatch,false,500).url " height="400" width="400" alt="">
-        </picture>
+        </picture> -->
+        <NuxtPicture
+          provider="imgix"
+          :src="props.article.eyecatch.url"
+          legacy-format="jpg"
+          fit="crop"
+          height="500"
+          width="500"
+          :img-attrs="{ class:'w-full', alt:`${props.article.title}のサムネイル画像`}"
+          :modifiers="{ format: 'webp', crop: 'squ' }"
+        />
       </CommonAppLink>
     </div>
     <div class="flex flex-col justify-between  md:h-48 p-0 m-0 md:px-1  md:py-4 grow">
