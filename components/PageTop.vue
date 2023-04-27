846d8c44e1154da34418f6696fbb14862163dc59
T.Y <tomyamamt@gmail.com>
2023-04-27 12:00:08 +0900

    位置を調整

--- components/PageTop.vue
+++ components/PageTop.vue
@@ -4,7 +4,7 @@
       <TopImgBlock :img="props.topImg" />
       <div v-if="props.title" class="absolute flex items-center justify-center w-full h-full title-box backdrop-blur-sm">
         <CommonContentWidthBox class="flex items-end justify-start">
-          <div class="flex flex-col w-full py-2 pl-4 my-2 md:px-16 md:py-6 mb-1/5h md:mb-1/7h bg-green/75 md:w-auto">
+          <div class="flex flex-col w-full py-2 pl-4 my-2 md:px-16 md:py-6 mb-1/6h md:mb-1/6h bg-green/75 md:w-auto">
             <AppHeading1>
               <span>{{ props.title }}</span>
             </AppHeading1>
