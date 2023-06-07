ec4f58bd9ab79c68eb3a233b174221727e4cf94f
TY <ddsamidare@gmail.com>
2023-06-08 02:01:57 +0900

    文字サイズ変更

--- components/AppHeading1.vue
+++ components/AppHeading1.vue
@@ -1,6 +1,6 @@
 <template>
   <h1
-    class="pl-2 text-4xl font-semibold text-white border-l-4 border-white border-solid"
+    class="pl-2 text-3xl font-semibold text-white border-l-4 border-white border-solid"
   >
     <slot />
   </h1>
