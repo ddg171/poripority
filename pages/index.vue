fe201932b443f271139d24ee879a836d1cc34ee2
T.Y <tomyamamt@gmail.com>
2023-08-22 19:28:40 +0900

    画像追加

--- pages/index.vue
+++ pages/index.vue
@@ -12,9 +12,11 @@
           </p>
         </div>
         <ArticleList :articles="latest" class="grid-cols-1 lg:grid-cols-2">
-          <div v-if="latest.length!==3" />
+          <div v-if="latest.length !== 3" />
           <div class="flex items-end justify-center w-full h-auto lg:max-w-xl md:h-full">
-            <div class="flex items-center justify-center w-full py-3 my-0 text-3xl readmore-link bg-green hover:bg-lightgreen focus:bg-lightgreen hover:underline">
+            <div
+              class="flex items-center justify-center w-full py-3 my-0 text-3xl readmore-link bg-green hover:bg-lightgreen focus:bg-lightgreen hover:underline"
+            >
               <CommonAppLink to="/blog" class="w-full h-full text-center">
                 記事一覧へ
               </CommonAppLink>
@@ -24,7 +26,7 @@
       </ContentSection>
       <ContentSection>
         <AppHeading2 class="mb-2">
-          About this site
+          Welcome!
         </AppHeading2>
         <div class="flex flex-col-reverse gap-4 lg:flex-row">
           <div class="flex flex-col items-center justify-center">
@@ -32,13 +34,47 @@
               src="/images/webp/shrimp.webp"
               legacy-format="jpeg"
               class="w-60 h-60"
-              :img-attrs="{ alt:'管理人の写真', height:240,width:240, decoding: 'async'}"
+              :img-attrs="{ alt: '管理人の写真', height: 240, width: 240, decoding: 'async' }"
             />
             <p class="w-full text-sm text-center">
               管理人の写真
             </p>
           </div>
           <div class="grid gap-2">
+            <CommonAppApra>
+              このWebサイトは"<span class="font-bold">Hata_kaze</span>"が趣味で色々フロントエンドの実験をしたり、ブログをやったりする個人サイトです。
+              以前はBloggerでブログをやっていましたが放置気味になってしまったので、心機一転ということでこちらに移転しました。
+            </CommonAppApra>
+            <CommonAppApra>
+              フロントエンドはNuxt/Vue(いずれもv3)+Typescript+Tailwind CSSで制作し、Google Cloudrun上にデプロイしています。
+              ドメインの接続と静的コンテンツ配信はFirebase Hosting、動的コンテンツについてはMicro CMSを利用しています。
+            </CommonAppApra>
+            <CommonAppApra>
+              世の中的には全部静的コンテンツにしてしまうSSGが流行りのようですが、このサイトはSSRモードで動作しています。
+            </CommonAppApra>
+            <CommonAppApra>
+              製作者のNuxtのSSRモードについての経験不足のため、このサイトは不思議な挙動をすることがありますがご容赦ください。
+            </CommonAppApra>
+          </div>
+        </div>
+      </ContentSection>
+      <ContentSection>
+        <AppHeading2 class="mb-2">
+          About this web site
+        </AppHeading2>
+        <div class="flex flex-col-reverse gap-4 lg:flex-row">
+          <div class="flex flex-col items-center justify-center w-1/2">
+            <NuxtPicture
+              src="/images/webp/diagram.webp"
+              legacy-format="jpeg"
+              class="w-96 h-96"
+              :img-attrs="{ alt: '当Webサイトの構成図', height: 400, width: 400, decoding: 'async' }"
+            />
+            <p class="w-full text-sm text-center">
+              当Webサイトの構成図
+            </p>
+          </div>
+          <div class="grid gap-2 w-1/2">
             <CommonAppApra>
               このWebサイトは"<span class="font-bold">Hata_kaze</span>"が趣味で色々フロントエンドの実験をしたり、ブログをやったりする個人サイトです。
               以前はBloggerでブログをやっていましたが放置気味になってしまったので、心機一転ということでこちらに移転しました。
@@ -65,10 +101,12 @@
           <div class="mx-0 my-4 md:my-2 md:mx-4">
             <div class="grid w-full gap-2">
               <CommonAppApra>
-                Wargame: Red dragonを遊ぶ人向けのDiscordサーバー。気がつけば開設からもう5年以上経過したが元気に運営中。流石に8年前のゲームがメインなので他のゲームも遊ぶ事が多いが、今でも呼びかけてみると対戦相手が見つかる程度には活発。
+                Wargame: Red
+                dragonを遊ぶ人向けのDiscordサーバー。気がつけば開設からもう5年以上経過したが元気に運営中。流石に8年前のゲームがメインなので他のゲームも遊ぶ事が多いが、今でも呼びかけてみると対戦相手が見つかる程度には活発。
               </CommonAppApra>
               <CommonAppApra>
-                なんだかんだ言いつつWargame: RDで対人戦をもっとも積極的にやっている日本人鯖の一つ。CPU相手の戦闘に飽きてきた方や「俺より強いやつに会いたい」という方にもおすすめ。最近は日本以外からもお客さんが来て賑やかになった。
+                なんだかんだ言いつつWargame:
+                RDで対人戦をもっとも積極的にやっている日本人鯖の一つ。CPU相手の戦闘に飽きてきた方や「俺より強いやつに会いたい」という方にもおすすめ。最近は日本以外からもお客さんが来て賑やかになった。
               </CommonAppApra>
               <CommonAppApra>
                 Wargameを含めて主に遊ばれているゲームは以下の通り。他にも色々なゲームを遊ぶことがあり、管理人が触ってないものもそこそこあったりする。
@@ -91,7 +129,7 @@ const { data, pending } = await useFetch('/api/blogs', { params: { limit: 3 } })
 
 const contents = data.value?.contents || []
 const latest = computed<Article[]>(() => data.value?.contents || [])
-const defaultContents:SliderContent[] = [
+const defaultContents: SliderContent[] = [
   {
     pic: {
       source: ['/images/webp/top-img01w640.webp 640w,/images/webp/top-img01w1270.webp 1024w,'],
@@ -148,13 +186,13 @@ const defaultContents:SliderContent[] = [
 const sliderContentsTemp = []
 if (contents.length) {
   const a = contents[0]
-  const eyecatch:Eyecatch = a.eyecatch
+  const eyecatch: Eyecatch = a.eyecatch
   const webp = eyecatch.url
   const w1280 = resizeWithTargetWidth(eyecatch, 1280, true).url
   const w640 = resizeWithTargetWidth(eyecatch, 640, true).url
   const jpg = eyecatch.url
 
-  const ArticleforSlider:SliderContent = {
+  const ArticleforSlider: SliderContent = {
     pic: {
       source: [`${w640} 640w,${w1280} 1024w,`],
       webp,
@@ -187,6 +225,4 @@ const games = ref<LinkParams[]>(
 
 </script>
 
-<style>
-
-</style>
+<style></style>
