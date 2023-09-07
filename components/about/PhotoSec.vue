8f9d9bc51f740c0d6a25b4fef20fac843417353c
T.Y <tomyamamt@gmail.com>
2023-09-07 17:53:18 +0900

    余白の調整と画像の追加

--- components/about/PhotoSec.vue
+++ components/about/PhotoSec.vue
@@ -6,7 +6,7 @@
       </AppHeading2>
     </template>
     <template #default>
-      <div class="flex flex-col items-center lg:items-start justify-center w-full text-white lg:flex-row ">
+      <div class="flex flex-col items-center lg:items-start justify-center w-full text-white lg:flex-row gap-4 lg:gap-8">
         <div class="relative pb-2  w-96 h-96 profile-pic bg-darkblue  overflow-hidden">
           <NuxtPicture
             :src="photo1"
@@ -36,16 +36,21 @@
             :img-attrs="{ class:'h-full w-full object-cover', alt:''}"
           />
         </div>
-        <div class="flex flex-col items-center flex-grow w-full  justify-center lg:justify-start  profile-data md:w-1/2 md:shrink-0">
+        <div class="flex flex-row items-center w-full  justify-center lg:justify-start  md:w-1/2 md:grow-0">
           <div class=" h-full flex flex-col items-start gap-2">
             <CommonAppApra>
               カメラ販売店の店員時代にα6000を購入して以来、カメラでの撮影をそのまま趣味として続けている。
+              あくまでも趣味のはずだったが職場で証明写真や集合写真の撮影を担当するようになったり、プロのカメラマンの手伝いをすることになったりするのだから世の中わからない。。
             </CommonAppApra>
             <CommonAppApra>
-              主な被写体は風景やスナップ写真。特に鳩を撮ることが多い。職場でも証明写真や集合写真の撮影を担当することもある。
+              使用機材はα6600がメイン。ソニーのカメラを使いはするがもっぱら静止画撮影が主で、動画撮影はあまりやっていない。
+              動画については「やって」と言われればやるけど、という程度。
             </CommonAppApra>
             <CommonAppApra>
-              最近は値下がりしていたDJI製のドローンを購入したため空撮もできるようになった。
+              主な被写体は風景やスナップ写真。特に鳩を撮ることが多い。鳩を撮るのはそこそこ動いて個性があり、何より肖像権がないから。
+            </CommonAppApra>
+            <CommonAppApra>
+              最近は値下がりしていたDJI製のドローンを購入したため空撮もできるようになった。現在飛べる場所を探しつつ練習中。
             </CommonAppApra>
           </div>
         </div>
--- pages/about.vue
+++ pages/about.vue
@@ -36,25 +36,36 @@
       <AboutPararaxSection />
     </ClientOnly>
     <AboutPhotoSec />
-    <AboutContentSection id="contact" class="z-30 py-20 min-h-3/4h bg-gray">
-      <template #heading>
-        <AppHeading2 class="z-40">
-          Contact
-        </AppHeading2>
-      </template>
-      <template #default>
-      <div class="z-30 flex flex-col items-center justify-center w-full h-full mt-16">
-        <div class="flex flex-col items-center justify-center w-full mb-4 text-2xl text-white">
-          If you have any questions, please contact me.
-        </div>
-
-        <div class="flex flex-col items-center justify-center gap-4 text-2xl text-white">
-          <ContactBox />
-        </div>
+    <section id="contact" class="w-full overflow-x-hidden z-30 bg-gray">
+      <NuxtPicture
+        src="/images/webp/about/contact.webp"
+        class="object-cover w-full h-40"
+        legacy-format="jpeg"
+        :img-attrs="{ class:'h-full w-full object-cover', alt:''}"
+      />
+      <IntersectionWrapper :threshold="0.99" trantision="none" class="w-full " @in="contactShow=true">
+        <div class="flex flex-col items-center w-full pt-4">
+          <CommonContentWidthBox class="flex flex-col px-2 ">
+            <div class="z-40 w-full mb-2 md:mb-8 ">
+              <AppHeading2 class="z-40">
+                Contact
+              </AppHeading2>
+            </div>
+          </CommonContentWidthBox>
+          <CommonContentWidthBox id="contact-info" class="flex flex-col px-2 " :is-show="contactShow">
+            <div class="z-30 flex flex-col items-center justify-center w-full mb-4">
+              <div class="flex flex-col items-center justify-center w-full mb-4 text-2xl text-white">
+                If you have any questions, please contact me.
               </div>
 
-      </template>
-    </AboutContentSection>
+              <div class="flex flex-col items-center justify-center gap-4 text-2xl text-white">
+                <ContactBox />
+              </div>
+            </div>
+          </CommonContentWidthBox>
+        </div>
+      </IntersectionWrapper>
+    </section>
   </div>
 </template>
 
@@ -64,6 +75,8 @@ const scrollOption = ref({ updateHistory: false, offset: -64 })
 const config = useRuntimeConfig()
 const title = 'About|' + config.public.siteName
 const description = 'profile, skill, contact'
+
+const contactShow = ref(false)
 setPageMetaData(
   title,
   description
@@ -91,5 +104,13 @@ onMounted(() => {
 .hero-enter-from,
 .hero-leave-to {
   @apply opacity-0;
+}
+
+#contact-info[is-show="false"]{
+  @apply opacity-0;
+}
+
+#contact-info{
+  @apply opacity-100 transition-opacity duration-200 delay-500;
 }
 </style>
