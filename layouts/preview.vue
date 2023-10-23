4ca63b169a1c46bfa5e23037cbe119ada1b3752d
TY <ddsamidare@gmail.com>
2023-10-23 21:19:41 +0900

    プレビューページを戻した

--- layouts/preview.vue
+++ layouts/preview.vue
@@ -1,19 +1,27 @@
 <template>
   <CommonLayoutBox id="layout" class="">
-    <PreviewAppHeader />
+    <AppHeader />
     <div class="flex flex-col items-center w-full min-h-screen">
-      <div id="top-box" class="w-full mb-4 h-1/4h lg:h-1/2h max-h-960p min-h-480p" />
+      <div id="top-box" class="w-full mb-4 h-1/3h lg:h-3/4h max-h-960p min-h-240p">
+        <ClientOnly>
+          <PageTop :title="pageTitle.title" :top-img="pageTitle.topImg" :subtitles="pageTitle.subtitles" />
+        </ClientOnly>
+      </div>
       <CommonContentWidthBox class="flex flex-col items-center lg:justify-center lg:items-start lg:flex-row ">
         <main class="flex flex-col items-center w-full bg-transparent">
-          <slot />
+          <div v-show="!isLoading.isLoading" class="w-full">
+            <slot />
+          </div>
+          <div v-if="isLoading.isLoading" class="w-full">
+            <ContentSection class="text-white text-2lg">
+              <PlaceHolder />
+            </ContentSection>
+          </div>
         </main>
-        <div id="side" class="sticky top-0 w-full mx-0 lg:w-96 lg:mx-1 shrink-0">
+        <div id="side" class="sticky top-0 w-full mx-0 lg:w-96 lg:mx-4 shrink-0">
           <aside class="flex flex-col-reverse w-full mb-2">
-            <AsideContentsBox class="mb-2">
+            <AsideContentsBox id="category" class="mb-2">
               <AppHeading3>カテゴリ</AppHeading3>
-              <ClientOnly>
-                <CategoryList />
-              </ClientOnly>
             </AsideContentsBox>
             <div id="side-contents" class="grid" />
           </aside>
@@ -29,6 +37,10 @@
 <script lang="ts" setup>
 const isBottomBtnShow = ref<boolean>(false)
 const { state: rootRect } = useRootRectStore()
+const { state: isLoading } = useLoadingStore()
+
+// ページ上部
+const { state: pageTitle } = usePageTopStore()
 
 watch(rootRect, (b) => {
   const top = b.top
