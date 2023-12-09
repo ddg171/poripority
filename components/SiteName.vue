803922738d8d7433540d3b5e41ab5baf3a239677
TY <ddsamidare@gmail.com>
2023-12-09 15:32:17 +0900

    タグの出し分けを実装

--- components/SiteName.vue
+++ components/SiteName.vue
@@ -1,13 +1,18 @@
 <template>
-  <h1 id="site-name" class="flex items-center justify-center ml-1 text-xl italic text-white site-name md:text-2xl md:ml-6">
+  <component :is="isTop?'h1':'div'" id="site-name" class="flex items-center justify-center ml-1 text-xl italic text-white site-name md:text-2xl md:ml-6">
     <a href="/">
       {{ siteName }}
     </a>
-  </h1>
+  </component>
 </template>
 
 <script setup lang="ts">
 const config = useRuntimeConfig()
+const route = useRoute()
+
+const isTop = computed<boolean>(() => {
+  return route.path === '/'
+})
 
 const siteName = computed<string>(() => {
   return (config.public.siteName || 'The hut of Poripority').toUpperCase()
