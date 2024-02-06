e0d95c74a41bc999b563b35f832ae8be56a6c881
TY <ddsamidare@gmail.com>
2024-02-07 00:24:00 +0900

    広告のリンク部分をコンポーネント化

--- components/AdCard.vue
+++ components/AdCard.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="flex items-start flex-row bg-white">
-    <div class=" overflow-hidden h-28 w-28  md:h-40 md:w-40 shrink-0 bg-darkblue">
+    <div class=" overflow-hidden h-28 w-28  md:h-36 md:w-36 shrink-0 bg-darkblue">
       <NuxtPicture
         v-if="props.ads.img?.url"
-        class="article-thumb w-full border-solid border-black opacity-100 transition-opacity duration-500"
+        class="article-thumb w-full opacity-100 transition-opacity duration-500"
         provider="imgix"
-        :src="props.ads.img.url||``"
+        :src="props.ads.img.url || ``"
         format="webp"
         legacy-format="jpeg"
-        fit="crop"
-        height="200"
-        width="200"
-        :img-attrs="{ class:'w-full bg-lightgreen/25', alt:`${props.ads.name}のサムネイル画像`,height:200,width:200, decoding: 'async',loading: 'lazy'}"
-        :modifiers="{q: 50}"
+        fit="clamp"
+        height="150"
+        width="150"
+        :img-attrs="{ class: 'w-full bg-lightgreen/25', alt: `${props.ads.name}のサムネイル画像`, height: 150, width: 150, decoding: 'async', loading: 'lazy' }"
+        :modifiers="{ h:150,w:150, q: 10 }"
       />
       <div v-else class="w-full h-full flex justify-center items-center text-sm text-white underline">
         {{ msg }}
@@ -20,22 +20,22 @@
     </div>
     <div class="flex flex-col justify-between py-1 m-0 md:h-36 md:px-2 grow">
       <div class="w-full ">
-        <div class="mb-0 text-sm font-semibold">
+        <div class="mb-0 text-xs md:text-sm font-semibold">
           <h3>
             <CommonAppLink :to="ads.amazon" class="text-black">
               {{ props.ads.name }}
+              <Icon name="mdi:open-in-new" />
             </CommonAppLink>
           </h3>
         </div>
       </div>
       <div class="w-full flex">
-        <a :href="props.ads.amazon" class="flex items-center justify-center p-2 mx-1 text-xl bg-amazonorange text-amazongray hover:opacity-75">
-          amazon
-        </a>
-        <a class="flex items-center justify-center p-2 mx-1 text-base  bg-darkblue">
-
+        <AdLinkBtn :href="props.ads.amazon" class="bg-amazonorange text-amazongray">
+          Amazon
+        </AdLinkBtn>
+        <AdLinkBtn v-if="props.ads.rakuten" :href="props.ads.rakuten" class="bg-red text-white">
           楽天
-        </a>
+        </AdLinkBtn>
       </div>
     </div>
   </div>
@@ -43,8 +43,8 @@
 
 <script setup lang="ts">
 import { Ads } from '~~/types/articles'
-interface Props{
-    ads:Ads
+interface Props {
+  ads: Ads
 }
 
 const props = defineProps<Props>()
@@ -54,6 +54,4 @@ const msg = computed<string>(() => {
 })
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
