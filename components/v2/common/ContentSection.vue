f5b607bd1507b6b97f1f10a01aeafcfe93f4aff5
TY <ddsamidare@gmail.com>
2026-09-24 12:48:51 +0900

    [refactor]コンポーネントの切り分け

---
+++ components/v2/common/ContentSection.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="w-full p-4 xl:px-2  flex flex-col gap-4">
+        <div class="pb-2  border-b border-lightgreen border-solid">
+            <div class="flex items-center gap-2">
+                <span :class="`block h-5 w-5 ${props.isImportant ? 'bg-orange' : 'bg-lightgreen'}`" />
+                <h2 class="block text-2xl font-semibold text-white ">
+                    {{ props.headerText }}
+                </h2>
+                <slot name="header" />
+            </div>
+        </div>
+        <div>
+            <slot name="content" />
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+    isImportant?: boolean
+    headerText: string
+}>(), {
+    isImportant: false
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,14 @@
     <TopSlider :slider-contents="topContents" :duration="5000" />
     <CommonContentWidthBox class="flex flex-col items-center ">
       <!-- 新規コンポーネント -->
-      <section class="w-full p-4 xl:px-2  flex flex-col gap-8">
-        <div class="pb-2  border-b border-lightgreen border-solid">
-          <div class="flex items-center gap-2">
-            <span class="block h-5 w-5 bg-orange" />
-            <h2 class="block text-2xl font-semibold text-white ">
-              Latest Articles
-            </h2>
-            <NuxtLink to="/blog" class="block p-1 ml-auto text-md text-lightgray hover:bg-lightgreen/25 text-middle">
-              <Icon name="akar-icons:arrow-right" class="inline-block w-3 h-3 mr-1" />
-              See All
-            </NuxtLink>
-          </div>
-        </div>
-        <div>
+      <V2CommonContentSection header-text="Latest Articles" :is-important="true">
+        <template #header>
+          <NuxtLink to="/blog" class="block p-1 ml-auto text-md text-lightgray hover:bg-lightgreen/25 text-middle">
+            <Icon name="akar-icons:arrow-right" class="inline-block w-3 h-3 mr-1" />
+            See All
+          </NuxtLink>
+        </template>
+        <template #content>
           <ul class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
             <li v-for="a in latestArticles" :key="a.id">
               <!-- 新記事カード -->
@@ -42,82 +36,68 @@
 
             </li>
           </ul>
-        </div>
-      </section>
-
-      <section class="w-full p-4 xl:px-2  flex flex-col gap-8">
-        <div class="pb-2  border-b border-lightgreen border-solid">
-          <div class="flex items-center gap-2">
-            <span class="block h-5 w-5 bg-lightgreen" />
-            <h2 class="block text-2xl font-semibold text-white ">
-              Welcome!
-            </h2>
-          </div>
-        </div>
-        <div class=" bg-darkblue border border-lightgreen">
-          <div class="flex flex-col-reverse gap-2 lg:flex-row items-center md:items-start">
-            <div class="bg-white w-72 h-72 shrink-0">
-              <div class="relative w-72 h-full">
-                <NuxtPicture src="/images/webp/shrimp.webp" legacy-format="jpeg" class="absolute top-0 left-0"
-                  :img-attrs="{ alt: '管理人の写真', height: 288, width: 288, decoding: 'async' }" />
-                <p class="absolute bottom-0 left-0 block  w-full text-sm text-center bg-darkblue/75 py-1">
-                  管理人の写真
-                </p>
+        </template>
+      </V2CommonContentSection>
+      <V2CommonContentSection header-text="Welcome!" :is-important="false">
+        <template #content>
+          <div class=" bg-darkblue border border-lightgreen">
+            <div class="flex flex-col-reverse gap-2 lg:flex-row items-center md:items-start">
+              <div class="bg-white w-72 h-72 shrink-0">
+                <div class="relative w-72 h-full">
+                  <NuxtPicture src="/images/webp/shrimp.webp" legacy-format="jpeg" class="absolute top-0 left-0"
+                    :img-attrs="{ alt: '管理人の写真', height: 288, width: 288, decoding: 'async' }" />
+                  <p class="absolute bottom-0 left-0 block  w-full text-sm text-center bg-darkblue/75 py-1">
+                    管理人の写真
+                  </p>
+                </div>
+              </div>
+              <div class="flex flex-col items-start gap-1 p-4 grow">
+                <CommonAppApra>
+                  "The hut of Poripority"にようこそ。このWebサイトは"<span
+                    class="font-bold">Hata_kaze</span>"が趣味で色々フロントエンドの実験をしたり、ブログをやったりする個人サイトです。
+                  以前はBloggerでブログをやっていましたが放置気味になってしまったので、心機一転ということでこちらに移転しました。
+                </CommonAppApra>
+                <CommonAppApra>
+                  技術的な内容はおそらくQitta等に投稿しますが、それ以外の雑多な話はこちらで更新予定です。
+                </CommonAppApra>
               </div>
             </div>
-            <div class="flex flex-col items-start gap-1 p-4 grow">
-              <CommonAppApra>
-                "The hut of Poripority"にようこそ。このWebサイトは"<span
-                  class="font-bold">Hata_kaze</span>"が趣味で色々フロントエンドの実験をしたり、ブログをやったりする個人サイトです。
-                以前はBloggerでブログをやっていましたが放置気味になってしまったので、心機一転ということでこちらに移転しました。
-              </CommonAppApra>
-              <CommonAppApra>
-                技術的な内容はおそらくQitta等に投稿しますが、それ以外の雑多な話はこちらで更新予定です。
-              </CommonAppApra>
-            </div>
           </div>
-        </div>
-      </section>
-
-      <section class="w-full p-4 xl:px-2  flex flex-col gap-8">
-        <div class="pb-2  border-b border-lightgreen border-solid">
-          <div class="flex items-center gap-2">
-            <span class="block h-5 w-5 bg-lightgreen" />
-            <h2 class="block text-2xl font-semibold text-white ">
-              About this web site
-            </h2>
-          </div>
-        </div>
-        <div class=" bg-darkblue border border-lightgreen">
-          <div class="flex flex-col-reverse gap-2 lg:flex-row items-center md:items-start">
-            <div class=" flex items-center justify-center">
-              <div class="relative w-72 h-72 sm:w-96 sm:h-96   shrink-0">
-                <NuxtPicture src="/images/webp/diagram.webp" legacy-format="jpeg" class="diagram"
-                  :img-attrs="{ alt: '当Webサイトの構成図', height: 540, width: 540, decoding: 'async' }" />
-                <p class="block absolute bottom-0 left-0 w-full text-sm text-center bg-darkblue/50 py-1">
-                  当Webサイトの構成図
-                </p>
+        </template>
+      </V2CommonContentSection>
+      <V2CommonContentSection header-text="About this web site" :is-important="false">
+        <template #content>
+          <div class=" bg-darkblue border border-lightgreen">
+            <div class="flex flex-col-reverse gap-2 lg:flex-row items-center md:items-start">
+              <div class=" flex items-center justify-center">
+                <div class="relative w-72 h-72 sm:w-96 sm:h-96   shrink-0">
+                  <NuxtPicture src="/images/webp/diagram.webp" legacy-format="jpeg" class="diagram"
+                    :img-attrs="{ alt: '当Webサイトの構成図', height: 540, width: 540, decoding: 'async' }" />
+                  <p class="block absolute bottom-0 left-0 w-full text-sm text-center bg-darkblue/50 py-1">
+                    当Webサイトの構成図
+                  </p>
+                </div>
+              </div>
+              <div class="flex flex-col items-start gap-1 p-4">
+                <CommonAppApra>
+                  フロントエンドはNuxt/Vue(いずれもv3)+Typescript+Tailwind CSSで制作しています。
+                  Nuxt3になってから開発サーバの起動、ビルドが非常に高速になり、非常に快適な開発環境になりました。
+                </CommonAppApra>
+                <CommonAppApra>
+                  バックエンドについてはGCP上にデプロイされています。Firebase Hostingを使用しており、動的コンテンツのみGoogle
+                  Cloudrun上で動作しているNuxt(SSRモード)にリダイレクトしています。
+                </CommonAppApra>
+                <CommonAppApra>
+                  ブログ部分のコンテンツについてはMicroCMSを使用しています。画像APIとNuxt/imgモジュールの連携も可能で非常に便利です。
+                </CommonAppApra>
+                <CommonAppApra>
+                  CI/CD(Github Actions)を導入しており、Mainブランチに変更内容がPushされると自動でビルド・デプロイが行われます。
+                </CommonAppApra>
               </div>
             </div>
-            <div class="flex flex-col items-start gap-1 p-4">
-              <CommonAppApra>
-                フロントエンドはNuxt/Vue(いずれもv3)+Typescript+Tailwind CSSで制作しています。
-                Nuxt3になってから開発サーバの起動、ビルドが非常に高速になり、非常に快適な開発環境になりました。
-              </CommonAppApra>
-              <CommonAppApra>
-                バックエンドについてはGCP上にデプロイされています。Firebase Hostingを使用しており、動的コンテンツのみGoogle
-                Cloudrun上で動作しているNuxt(SSRモード)にリダイレクトしています。
-              </CommonAppApra>
-              <CommonAppApra>
-                ブログ部分のコンテンツについてはMicroCMSを使用しています。画像APIとNuxt/imgモジュールの連携も可能で非常に便利です。
-              </CommonAppApra>
-              <CommonAppApra>
-                CI/CD(Github Actions)を導入しており、Mainブランチに変更内容がPushされると自動でビルド・デプロイが行われます。
-              </CommonAppApra>
-            </div>
           </div>
-        </div>
-      </section>
+        </template>
+      </V2CommonContentSection>
     </CommonContentWidthBox>
   </div>
 </template>
